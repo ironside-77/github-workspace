b377d7f8ed045dcba7be41f4600a52f605beaa58
Marcelo <ironside772@gmail.com>
2025-07-22 14:05:10 -0400

    more stuff yet again

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,23 +1,35 @@
 from prefect import task, flow
 import pandas as pd
+from prefect_github.repository import GitHubRepository
+from io import StringIO
+
+git = GitHubRepository.load("gitblock")
+
 
 
 @task(log_prints=True)
 def importing_data():
-    data = pd.read_csv('data.csv')
-    print('running task 1')
+    print("running task 1. ----------Attempting to import data---------- !")
+
+    csv_bytes = git.read_path('data.csv')
+    csv_string = csv_bytes.decode("utf-8")
+    
+    
+    data = pd.read_csv(StringIO(csv_string))
+    
     return data
 
 @task(log_prints=True)
-def to_json(data):
-    print("Running task 2")
-    data.to_json('data.json', orient='records')
+def load(df):
+    print("Running task 2 -----------Attempting to load data-------------")
+    csv_str = df.to_json()
+    git.write_path(path='data.json', content=csv_str)
 
 @flow(log_prints=True)
 def main():
     print("running main flow")
     data = importing_data()
-    to_json(data)
+    load(data)
 
 
 
